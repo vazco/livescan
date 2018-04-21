370284906d3a0a4bfbaff4d64ac71122003a87d9
Magikis <kulczakkacper@gmail.com>
2018-04-21 19:59:47 +0200

    starting of db

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,25 @@
+import {DynamoDB} from 'aws-sdk';
+
 import {Config} from '../types';
+import {APIGatewayEvent, Callback, Context} from 'aws-lambda';
+import {notify} from '../notificators';
+import {runTesters} from '../testers';
+
+ const doubleFunction = async () => {
+    const params = {
+        TableName : "Services",
+        Item: {
+            _id: {S: 'abc1'},
+            name: {S: "Stare-BI"}
+        }
+    }
+    const ddb = new DynamoDB();
+    ddb.putItem(
+            )
+
+    return await ddb.putItem(params).promise()
+    // const ddb = new DynamoDB().promise()
+ }
 
 export const getConfig = async (): Promise<Config> => {
     return {
@@ -19,4 +40,25 @@ export const getConfig = async (): Promise<Config> => {
             },
         ],
     };
+};
+
+
+export default async (event: APIGatewayEvent, context: Context, cb: Callback) => {
+    try {
+
+        const res = await doubleFunction();
+        console.log(res)
+        cb(null, {
+            body: JSON.stringify({
+                message: JSON.stringify(res)
+            }),
+            statusCode: 200,
+        });
+    } catch (err) {
+        console.error(err);
+        cb(null, {
+            body: JSON.stringify(err),
+            statusCode: 500,
+        });
+    }
 };
