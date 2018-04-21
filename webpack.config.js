ff83a0ee3560378b6f66bfb0e93dca24e2116759
Maciek Stasieluk <macrusher@gmail.com>
2018-04-21 19:26:20 +0200

    Testers WIP

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,27 @@ const path = require('path');
 const slsw = require('serverless-webpack');
 
 module.exports = {
-  entry: slsw.lib.entries,
-  devtool: 'source-map',
-  resolve: {
-    extensions: [
-      '.js',
-      '.jsx',
-      '.json',
-      '.ts',
-      '.tsx'
-    ]
-  },
-  output: {
-    libraryTarget: 'commonjs',
-    path: path.join(__dirname, '.webpack'),
-    filename: '[name].js',
-  },
-  target: 'node',
-  module: {
-    loaders: [
-      { test: /\.ts(x?)$/, loader: 'ts-loader' },
-    ],
-  },
+    entry: slsw.lib.entries,
+    devtool: 'source-map',
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx',
+            '.json',
+            '.ts',
+            '.tsx'
+        ],
+        modules: [path.resolve('.'), 'node_modules'],
+    },
+    output: {
+        libraryTarget: 'commonjs',
+        path: path.join(__dirname, '.webpack'),
+        filename: '[name].js',
+    },
+    target: 'node',
+    module: {
+        loaders: [
+            {test: /\.ts(x?)$/, loader: 'ts-loader'},
+        ],
+    },
 };
