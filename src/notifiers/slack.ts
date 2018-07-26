bea5197ca4ba1cf5b33d4a576c850ef4afc9f09a
Krzysztof Szut <szut.krzysztof@gmail.com>
2018-07-26 13:27:25 +0200

    Added slack webhook url as a process.env variable

--- src/notifiers/slack.ts
+++ src/notifiers/slack.ts
@@ -5,7 +5,8 @@ export default (results: IServiceCheckResult[]): Promise<any> => {
   const resultsParsed: string[] = results.map(
     ({ isOk, name }: IServiceCheckResult) => `• ${name}: ${isOk ? '*Success*' : '*Failure*'}\n`
   )
-  return fetch('https://hooks.slack.com/services/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', {
+
+  return fetch(process.env.SLACK_WEBHOOK_URL, {
     body: JSON.stringify({
       text: `*Vazco service health check has been run*\nThese are the results:\n${resultsParsed}`
     }),
